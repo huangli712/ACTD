d2a957658cd101dff52967ac7a672df07e08a4f0
Li Huang <lihuang.dmft@gmail.com>
2025-07-17 20:32:57 +0800

    fix gen.sh

--- util/gen.sh
+++ util/gen.sh
@@ -2,13 +2,20 @@
 
 dir=`ls ../src`
 ACGEN=~/Working/devel/ACTest/util/acgen.jl
+ACMAT=~/Working/devel/ACTest/util/acmat.jl
+OFFSTR="offdiag"
 
 for d in $dir
 do
     echo $d
     cd ../src/$d
     pwd
-    $ACGEN act.toml
+    if [[ $d =~ $OFFSTR ]]
+    then
+        $ACMAT act.toml
+    else
+        $ACGEN act.toml
+    fi
     cd ..
     tar -czvf $d.tar.gz $d
     cd ../util
