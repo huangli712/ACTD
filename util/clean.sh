e6c5fe21387c85da6a9a5392d229e21a9f42d8a8
Li Huang <lihuang.dmft@gmail.com>
2025-07-20 23:52:21 +0800

    fix clean.sh

--- util/clean.sh
+++ util/clean.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#
+# This script will clean the src directory, removing all the green.* and
+# image.* files. 
+#
+# This script is extremely dangerous. please use it at your own risk!
+#
+
 dir=`ls ../src`
 
 for d in $dir
