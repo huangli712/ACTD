c0a92d9ed00b7db93544d91148dfd2aa4829b86a
lihuang <huangli@caep.cn>
2025-09-10 00:47:32 +0800

    fix access.jl

--- util/access.jl
+++ util/access.jl
@@ -21,16 +21,21 @@ function read_green(dir::String, type::String, ind::Int)
     else
         fn = joinpath(dir, "green.bin.") * string(ind)
         if isfile(fn)
+            # Read the green's functions
             dlm = readdlm(fn, comments = true)
+
+            # Determine number of data bins and number of time slices 
             nline1 = countlines(fn)
             nline2, = size(dlm)
             nbins = (nline1 - nline2) ÷ 3
             ntime = nline2 ÷ nbins
             @assert nbins ≥ 1
             @assert ntime ≥ 1
-            return reshape(dlm[:,1], (ntime, nbins)),
-                   reshape(dlm[:,2], (ntime, nbins)),
-                   reshape(dlm[:,3], (ntime, nbins))
+
+            # Reshape the data and return them
+            return reshape(dlm[:,1], (ntime, nbins)), # τ
+                   reshape(dlm[:,2], (ntime, nbins)), # G(τ)
+                   reshape(dlm[:,3], (ntime, nbins))  # ΔG(τ)
         else
             @error "File $fn doesn't exist!"
         end
@@ -49,7 +54,7 @@ end
 
 #mesh, image = read_image("../src/mat_boson_cont_n1e-3", 10)
 read_green("../src/mat_boson_cont_n1e-3/", "matsubara", 100)
-time = read_green("../src/tau_boson_cont_n1e-3/", "time", 100)
-@show time[:,1]
-@show time[:,2]
-@show time[:,100]
+time, green, error = read_green("../src/tau_boson_cont_n1e-3/", "time", 100)
+@show green[:,1]
+@show green[:,2]
+@show green[:,100]
