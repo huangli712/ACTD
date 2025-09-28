c073ac4ceb47132c6a41c857e35ac47ec629118d
Li Huang <lihuang.dmft@gmail.com>
2025-09-28 15:41:39 +0800

    fix make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,22 @@
 using Documenter
 
 makedocs(
-    sitename = "ACTD",
-    clean = false,
+    sitename = "ACTD: The User Guide",
+    clean = true,
     authors = "Li Huang <huangli@caep.cn> and contributors",
     format = Documenter.HTML(
         prettyurls = false,
         ansicolor = true,
         repolink = "https://github.com/huangli712/ACTD",
+        size_threshold = 409600, # 400kb
         assets = ["assets/actd.css"],
         collapselevel = 1,
     ),
     remotes = nothing,
+    #format = Documenter.LaTeX(platform = "none"),
+    modules = Module[],
     pages = [
-        "Home" => "index.md",
+        "Welcome" => "index.md",
         "Introduction" => "intro.md",
         "Method" => "method.md",
         "Datasets" => "data.md",
